d417ecd5de98256d69a7552f47718e2b303b24e9
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-11-28 18:29:38 +0200

    Adding MVC pattern to the project, adding rendering recipe logic.

---
+++ src/js/model.js
@@ -0,0 +1,32 @@
+import { async } from 'regenerator-runtime';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const currentData = await response.json();
+    if (!response.ok) {
+      throw new Error(`${currentData.message} (${response.status})`);
+    }
+    console.log(currentData);
+    const { recipe } = currentData.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
